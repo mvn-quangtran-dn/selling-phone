b49aa2b844100e89807eba31f0a9ac76ee5a5fbc
letanhienth92 <uthienth92@gmail.com>
2018-09-13 19:29:57 +0700

    tao database-model-controller

---
+++ database/migrations/2018_09_13_081732_create_order_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quantity');
+            $table->integer('product_id')->nullable()->unsigned();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->integer('order_id')->nullable()->unsigned();
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_details');
+    }
+}
